7bc68aafc3be65aca4afd9b770fc6c93124e5bd0
Gregor G <design@gresak.net>
2014-05-02 16:38:43 +0200

    added discuss widget

--- PHaiku/Haiku.php
+++ PHaiku/Haiku.php
@@ -29,6 +29,7 @@ class Haiku extends PHaiku {
 		$widgets->langmenu = $this->langMenu($this->app->config("languages"));
 		$widgets->haiku = $this->haikuWidget();
 		$widgets->twitter = $this->twitterWidget();
+		$widgets->discuss = $this->discussWidget();
 		return $widgets;
 	}
 	
@@ -56,6 +57,17 @@ class Haiku extends PHaiku {
 	public function twitterWidget() {
 		return '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/gresakg" data-widget-id="461454369859174400">Tweets by @gresakg</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+	}
+	
+	public function discussWidget() {
+		$filename = $this->getFilepath("_discuss",$this->lang,"php");
+		if(file_exists($filename)) {
+			return file_get_contents($filename);
+		}
+		else {
+			return "";
+		}
+		
 	}
 
 	
