610f897a8c9044949c33fc8c196b71a6630013e9
Gregor G <design@gresak.net>
2014-05-02 22:31:46 +0200

    replaced few widgets with one text widget

--- templates/skela/footer.php
+++ templates/skela/footer.php
@@ -1,6 +1,6 @@
 <footer><p class='fourteen columns'>(c) 2014 <a href='http://gresak.net'>gresak.net</a></p><p class='four columns'> Powered by <em>Phaiku</em>.</p></footer>
 </div><!-- container -->
-<?php $widgets->forkme; ?>
+<?php echo $widgets->forkme; ?>
 <script src="<?php echo $template_url; ?>/js/dropdowns.min.js"></script>
 <script src="<?php echo $template_url; ?>/js/script.js"></script>
 <?php echo $widgets->analytics; ?>
