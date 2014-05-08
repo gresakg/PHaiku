6cde7c4432a0a663901694c54e37c2194908c072
Gregor G <design@gresak.net>
2014-05-08 11:19:05 +0200

    added config sample to start up fast

---
+++ config-sample/config.php
@@ -0,0 +1,93 @@
+<?php
+return [
+	"mode" => "developement",
+	"debug" => 'true',
+	"templates.path" => "./templates/skela",
+	"class.name" => "\PHaiku\Haiku",
+	"data.store" => "./data",
+	"default.ext" => "html",
+	"multilingual" => true,
+	"default.language" => "en",
+	"languages" => ["sl","en"],
+	//setup default widgets
+	"widgets" => [
+		"menu" => [
+			"handler"=>"setMenu",
+			"arguments" => NULL
+		],
+		"langmenu" => [
+			"handler"=>"langMenu",
+			"arguments" => NULL
+		],
+		"haiku" => [
+			"handler"=>"haikuWidget",
+			"arguments" => NULL
+		],
+		"twitter" => [
+			"handler"=>"textWidget",
+			"arguments" => ["widgets/twitter"]
+		],
+		"discuss" => [
+			"handler"=>"textWidget",
+			"arguments" => ["widgets/discuss"]
+		],
+		"analytics" => [
+			"handler"=>"textWidget",
+			"arguments" => ["widgets/analytics"]
+		],
+		"forkme" => [
+			"handler"=>"textWidget",
+			"arguments" => ["widgets/forkme"]
+		],
+		"news" => [
+			"handler"=>"newsWidget",
+			"arguments"=>[5]
+		],
+	],
+	//define basic routes
+	"routes" => [
+		[
+			"name" => "index",
+			"route" => "/",
+			"method" => "get",
+			"handler" => "frontPage",
+		],
+		[
+			"name" => "page",
+			"route" => "/p/:page+",
+			"method" => "get",
+			"handler" => "setPage",
+		],	
+	//custom routes
+		[
+			"name" => "news",
+			"route" => "/news",
+			"method" => "get",
+			"handler" => "theNews",
+		],
+		[
+			"name" => "newsitem",
+			"route" => "/n/:segments+",
+			"method" => "get",
+			"handler" => "newsItem",
+		],
+		[
+			"name" => "contact",
+			"route" => "/contact",
+			"method" => "get",
+			"handler" => "contactForm",
+		],
+		[
+			"name" => "postcontact",
+			"route" => "/contact/:token",
+			"method" => "post",
+			"handler" => "contactForm",
+		],
+	],
+	// define default route conditions
+	"route.conditions" => [
+		'page+' => '[a-zA-Z0-9_-]+',
+		'lang' => '[\w]{2}',
+	],
+];
+
