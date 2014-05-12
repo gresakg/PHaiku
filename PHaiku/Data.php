5e24dce8adec7c3f907365ab5e4d5d2a2b924010
Gregor G <design@gresak.net>
2014-05-12 10:30:18 +0200

    added extended stdClass Data

---
+++ PHaiku/Data.php
@@ -0,0 +1,15 @@
+<?php
+namespace PHaiku;
+
+class Data extends stdClass {
+	
+	public function __get($name) {
+		return "";
+	}
+	
+	public function addChild($name) {
+		$this->$name = new \PHaiku\Data();
+	}
+	
+}
+
