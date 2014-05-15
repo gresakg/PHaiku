175f1ae46b8feb0755360aa59afc2f2b57c0b414
Gregor G <design@gresak.net>
2014-05-15 13:24:01 +0200

    contact works with pear smtp mailer

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ include __DIR__.'/config/services.php';
 
 \PHaiku\PHaiku::$basedir = __DIR__;
 
-\PHaiku\PHaiku::$version = "0.51.20.14";
+\PHaiku\PHaiku::$version = "0.60.20.14";
 
 \PHaiku\PHaiku::setRoutes($di);
 
